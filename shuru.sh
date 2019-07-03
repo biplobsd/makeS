72bbb127d28bccdbe692510e54517de3c3f2e787
biplobsd <biplobsd11@gmail.com>
2019-07-04 00:18:24 +0600

    put command in function

--- shuru.sh
+++ shuru.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
-    sudo -s
-    passwd --lock root
-    passwd -d root
-    sudo passwd root
-now
-now
-    sudo apt-get --yes --force-yes update && sudo apt-get --yes --force-yes install libcrypto++-dev libc-ares-dev libcurl4-openssl-dev libfreeimage3 libfreeimage-dev git autoconf autogen build-essential && sudo pip install -r https://raw.githubusercontent.com/r0oth3x49/lynda-dl/master/requirements.txt
+
+chRpass () {
+	sudo -s ;
+	passwd --lock root ;
+	passwd -d root ;
+	sudo passwd root ;
+	now
+	now
+}
+
+pgInstalling () {
+	sudo apt-get --yes --force-yes update && sudo apt-get --yes --force-yes install libcrypto++-dev libc-ares-dev libcurl4-openssl-dev libfreeimage3 libfreeimage-dev git autoconf autogen build-essential && sudo pip install -r https://raw.githubusercontent.com/r0oth3x49/lynda-dl/master/requirements.txt
 	wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
+}
+
+cloneProject () {
 	git clone https://github.com/meganz/MEGAcmd.git
-    git clone https://github.com/r0oth3x49/lynda-dl.git
+	git clone https://github.com/r0oth3x49/lynda-dl.git
+}
+
+setupProject () {
+	# MEGAcmd START #
     cd MEGAcmd/
 	git submodule update --init --recursive
     sh autogen.sh
@@ -18,5 +30,17 @@ now
     sudo make install
     sudo ldconfig
     cd ~
+	# END #
+	
+	# Torrent CLI START #
 	go get github.com/anacrolix/torrent/cmd/torrent
+	npm install webtorrent-cli -g
+	# END #
+}
+
+chRpass
+# pgInstalling
+# cloneProject
+# setupProject
+
     echo -e "\x1B[01;91m This is your root password :\x1B[01;92m now \x1B[0m\x1B[0m"
