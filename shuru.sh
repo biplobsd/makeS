48198f46e0805e6f5c8e00c80999f7b2803b7f27
Biplob SD <43641536+biplobsd@users.noreply.github.com>
2019-07-04 17:22:22 +0600

    TEST 12

--- shuru.sh
+++ shuru.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-pgInstalling() {
+a() {
 	command sudo apt-get --yes --force-yes update && sudo apt-get --yes --force-yes install libcrypto++-dev libc-ares-dev libcurl4-openssl-dev libfreeimage3 libfreeimage-dev git autoconf autogen build-essential && sudo pip install -r https://raw.githubusercontent.com/r0oth3x49/lynda-dl/master/requirements.txt
 	command wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
 }
 
-cloneProject() {
+b() {
 	git clone https://github.com/meganz/MEGAcmd.git
 	git clone https://github.com/r0oth3x49/lynda-dl.git
 }
 
-setupProject() {
+c() {
 	# MEGAcmd START #
     cd MEGAcmd/
 	git submodule update --init --recursive
@@ -38,7 +38,7 @@ now
 now
 # END #
 
-pgInstalling
-cloneProject
-setupProject
+a
+b
+c
 echo -e "\x1B[01;91m This is your root password :\x1B[01;92m now \x1B[0m\x1B[0m"
