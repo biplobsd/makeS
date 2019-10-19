712925263fc8c8daaf69681df9d435548b6a6926
biplobsd <biplobsd11@gmail.com>
2019-10-19 22:49:41 +0600

    .deb by MEGAcmd install

--- shuru.sh
+++ shuru.sh
@@ -2,13 +2,13 @@
 
 function package_init()
 {
-	sudo apt-get --yes --force-yes update && sudo apt-get --yes --force-yes install python-pip libcrypto++-dev libc-ares-dev libcurl4-openssl-dev libfreeimage3 libfreeimage-dev git autoconf autogen build-essential && sudo pip install --quiet -r https://raw.githubusercontent.com/r0oth3x49/lynda-dl/master/requirements.txt
+	sudo apt-get --yes --force-yes update && sudo apt-get --yes --force-yes install python-pip libc-ares2 libc6 libcrypto++6 libgcc1 libmediainfo0v5 libpcre3 libpcrecpp0v5 libssl1.1 libstdc++6 libzen0v5 zlib1g apt-transport-https git && sudo pip install --quiet -r https://raw.githubusercontent.com/r0oth3x49/lynda-dl/master/requirements.txt
 	wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
 }
 
 function clone_pro()
 {
-	git clone https://github.com/meganz/MEGAcmd.git
+	wget -c -nc -O megacmd.deb https://mega.nz/linux/MEGAsync/Debian_9.0/amd64/megacmd-Debian_9.0_amd64.deb
 	git clone https://github.com/r0oth3x49/lynda-dl.git
 }
 
@@ -20,26 +20,7 @@ function setup_pro()
 	if [ $? -eq 0 ]; then
 		echo Mega-cmd is already installed.
 	else
-		cd MEGAcmd/
-		isfileX=( mega-cmd mega-cmd-server mega-exec )
-		for i in "${isfileX[@]}"
-		do
-			if [[ ! -f "$i" ]]; then
-				echo "$i not exist."
-				notexistF=true
-			fi
-		done
-		if $notexistF ; then
-			git submodule update --init --recursive
-			sh autogen.sh
-			./configure
-			make
-			sudo ldconfig
-		fi
-		sudo make install
-		sudo ldconfig
-		cd ~
-
+		sudo dpkg -i megacmd.deb
 		# Is MEGAcmd installed!
 		type mega-cmd
 		if [ ! $? -eq 0 ]; then
@@ -77,21 +58,8 @@ function __start()
 }
 __start
 #### Change root passward START ####
-#sudo -s
-#passwd --lock root
-#passwd -d root
 sudo passwd root
 now
 now
-# if [ $? -eq 0 ]; then
-# 	echo OK
-# else
-# 	sudo passwd --lock root
-# 	sudo passwd -d root
-# 	sudo passwd root
-# 	now
-# 	now
-# fi
-# END #
 echo -e "\x1B[01;91m This is your root password :\x1B[01;92m now \x1B[0m\x1B[0m"
 
